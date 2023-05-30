652b428c19d288e1f72896ffbfeee306cd7c4edc
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-05-31 01:01:31 +0300

    update

--- client-reactjs/src/App.js
+++ client-reactjs/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import PostCreate from "./PostCreate";
 import PostList from "./PostList";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="container">
+      <h1 id="title">
+        Building an Interactive Application with React and Node.js Using
+        Microservices Architecture
+      </h1>
+      <h2 id="by">İbrahim Fevzi Kayan (ibrahimfevzi@gmail.com)</h2>
       <h1>Create Post</h1>
       <PostCreate />
       <hr />
--- posts/index.js
+++ posts/index.js
@@ -38,6 +38,11 @@ app.post("/posts", async (req, res) => {
   res.status(201).send(posts[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
+});
+
 app.listen(4000, () => {
   console.log("Listening on 4000");
 });
